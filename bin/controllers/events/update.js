cdb010349969afd5c2a0d2f3711df4ee80edfd31
SwithFr <contact@swith.fr>
2016-04-01 17:51:14 +0200

    update event feature

--- bin/controllers/events/update.js
+++ bin/controllers/events/update.js
@@ -9,63 +9,36 @@ var jsonMiddlewares = require( "../../core/express/middlewares.js" ).json,
     Event = require( "../../core/sequelize.js" ).models.Event,
     EventUser = require( "../../core/sequelize.js" ).models.EventUser;
 
-// [PATCH] - /events/create
+// [PATCH] - /events/:id
 module.exports = function( oRequest, oResponse ) {
 
-    var oPOST = oRequest.body,
-        oEvent = Event.build(),
-        oEventUser = EventUser.build();
+    var oPOST = oRequest.body;
 
-    var iUserId = +oRequest.headers.userid;
-
-    oEvent.title = ( oPOST.title || "" ).trim();
-    oEvent.description = ( oPOST.description || "" ).trim();
-
-    oEvent
-        .validate()
-        .then( function( oValidationReport ) {
-            if( oValidationReport ) {
-                return jsonMiddlewares.error( oRequest, oResponse, oValidationReport.errors, 400 );
+    Event
+        .findOne( {
+            "where": {
+                "uuid": oRequest.params.id
             }
+        } )
+        .catch( function( oError ) {
+            return jsonMiddlewares.error( oRequest, oResponse, oError, 500 );
+        } )
+        .then( function( oEvent ) {
 
-            setEvent( oEvent, iUserId )
-                .save()
-                .catch( function( oError ) {
-                    return jsonMiddlewares.error( oRequest, oResponse, oError, 500 );
-                } )
-                .then( function( oSavedEvent ) {
-                    setEventUserRelation( oEventUser, iUserId, oSavedEvent.id )
-                        .save()
-                            .catch( function( oError ) {
-                                return jsonMiddlewares.error( oRequest, oResponse, oError, 500 );
-                            } )
-                            .then( function() {
-                                oSavedEvent && jsonMiddlewares.send( oRequest, oResponse, {
-                                    "id": oSavedEvent.id,
-                                    "title": oSavedEvent.title,
-                                    "uuid": oSavedEvent.uuid,
-                                    "user_id": oSavedEvent.user_id,
-                                    "description": oSavedEvent.description
-                                } );
-                            } );
-                } );
+            if ( oEvent ) {
+                oEvent.title = ( oPOST.title || "" ).trim();
+                oEvent.description = ( oPOST.description || "" ).trim();
+
+                oEvent
+                    .save()
+                    .catch( function( oError ) {
+                        return jsonMiddlewares.error( oRequest, oResponse, oError, 500 );
+                    } )
+                    .then( function( oUpdatedEvent ) {
+                        return jsonMiddlewares.send( oRequest, oResponse, oUpdatedEvent );
+                    } )
+            } else {
+                return jsonMiddlewares.error( oRequest, oResponse, "NO_EVENT_FOUND", 404 );
+            }
         } );
-};
-
-var setEventUserRelation = function( oEventUser, iUserId, iEventId ){
-    oEventUser.user_id = iUserId;
-    oEventUser.event_id = iEventId;
-    return oEventUser;
-};
-
-var setEvent = function( oEvent, iUserId ){
-    oEvent.uuid = generateFakeUuid();
-    oEvent.user_id = iUserId;
-    return oEvent;
-};
-
-var generateFakeUuid = function() {
-    return require( "zouti" )
-              .whirlpool( ( new Date() ).toString() )
-              .substring( 0, 8 )
 };
